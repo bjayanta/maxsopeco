1de7dcb8843c4eba57288239c8e0f665d45e0f48
Jayanta <bjayanta.me@gmail.com>
2022-02-27 22:06:38 +0600

    add create view

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import TaskDetail, TaskList
+from .views import TaskCreate, TaskDetail, TaskList
 
 urlpatterns = [
     path('', TaskList.as_view(), name='todo.index'),
     path('todo/<int:pk>/', TaskDetail.as_view(), name='todo.show'),
+    path('todo/create/', TaskCreate.as_view(), name='todo.create'),
 ]
